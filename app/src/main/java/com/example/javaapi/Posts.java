72629687c73e78814833bdf1fa7cdb46c28950a3
Naphtali-cpu <naphtali919@gmail.com>
2021-10-08 23:17:04 +0300

    Added first files

---
+++ app/src/main/java/com/example/javaapi/Posts.java
@@ -0,0 +1,19 @@
+package com.example.javaapi;
+
+public class Posts {
+    private String title;
+    private String body;
+
+    public Posts(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
